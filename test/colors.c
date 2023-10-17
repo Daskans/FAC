18d737a4ce96e77ed07cb9bbfd8890091bb01210
Kyrian Balavoine <kyrian.balavoine@etu.u-bordeaux.fr>
2023-10-17 17:09:12 +0200

    17/10/2023 (5)

---
+++ test/colors.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Colors*/
+#define BOLDGREEN "\033[1m\033[32m" 
+#define BOLDRED   "\033[1m\033[31m" 
+#define BOLDBLUE  "\033[1m\033[34m"
+#define BOLDLIGHTBLUE "\033[1m\033[94m"
+#define BOLDPINK "\033[1m\033[95m"
+#define BOLDORANGE "\033[1m\033[38;5;208m"
+#define BOLDDARKGREEN "\033[1m\033[38;5;22m"
+#define RESET     "\033[0m"         /* Reset color */
+
+void main(void) {
+    printf("%s %s\n", BOLDLIGHTBLUE "hello", BOLDGREEN "there");
+    printf("%s %s\n", BOLDORANGE "General", BOLDRED "Kenobi");
+}
