762608c9a34b85fdfaf53c5391d7b46e573c4490
Kyrian Balavoine <kyrian.balavoine@etu.u-bordeaux.fr>
2024-10-14 22:39:29 +0200

    14/10/2024

--- L3 Info/Programmation Fonctionnelle/7-Listes/feuille7.ml
+++ L3 Info/Programmation Fonctionnelle/7-Listes/feuille7.ml
@@ -112,3 +112,101 @@ let test =
         assert(terminal_reverse list = rev_list);
     end
 ;;
+
+(* EXERCICE 4 *)
+
+let rec iota_r n =
+    if n = 1 then
+        [1]
+    else
+        n :: (iota_r (n-1))
+
+(* O(2n) -> O(n) *)
+
+let rec iota n =
+    if n = 1 then
+        [1]
+    else
+        (iota (n-1)) @ [n]
+
+(* O(n²) *)
+
+let terminal_iota_r n =
+    let rec iota_r_aux k result =
+        if k = n then
+            k :: result
+        else
+            iota_r_aux (k+1) (k::result)
+    ;
+    in iota_r_aux 1 []
+
+let terminal_iota n =
+    let rec iota_aux n result =
+        if n = 1 then
+           1 ::  result
+        else
+            iota_aux (n-1) (n::result)
+    ;
+    in iota_aux n []
+
+let test =
+    begin
+        assert(iota_r 5 = [5;4;3;2;1]);
+        assert(iota 5 = [1;2;3;4;5]);
+        assert(terminal_iota_r 5 = [5;4;3;2;1]);
+        assert(terminal_iota 5 = [1;2;3;4;5]);
+    end
+;;
+
+(* 4.6) avec une complexité de O(n²), une latence se fait ressentire dès n = 4000 *)
+
+(* EXERCICE 5 *)
+
+let l_member = [1;2;3;4;5]
+
+let rec member x list =
+    match list with
+    | [] -> false
+    | head::tail -> if head = x then
+                        true
+                    else
+                        false || member x tail
+
+let test =
+    begin
+        assert(member 1 l_member = true);
+        assert(member 11 l_member = false);
+        assert(member 1 [] = false);
+    end
+;;
+
+(* 5.2) 'a -> 'a list -> bool *)
+
+(* 5.3) Le code est pratiquement le même à l'exeption de la comparaison head = x qui est une comparaison structurelle
+dans le mem original. Mon code est cependant identique à memq *)
+
+(* EXERCICE 6 *)
+
+let l_count = [1;1;1;2;3;1]
+
+let rec count x list =
+    match list with
+    | [] -> 0
+    | head::tail -> if head = x then
+                        1 + count x tail
+                    else
+                        count x tail
+
+
+(* 6.2) 'a -> 'a list -> int *)
+
+let member' x list = count x list > 0
+
+let test =
+    begin
+        assert(count 1 l_count = 4);
+        assert(count 4 l_count = 0);
+        assert(member' 1 l_count = true);
+        assert(member' 4 l_count = false);
+    end
+;;
