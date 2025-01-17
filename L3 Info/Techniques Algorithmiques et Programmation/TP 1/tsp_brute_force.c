17a4b301cb910eaa2e2a38984fd843f7c373a792
Kyrian Balavoine <kyrian.balavoine@etu.u-bordeaux.fr>
2025-01-17 12:09:18 +0100

    Compilation premier TD

---
+++ L3 Info/Techniques Algorithmiques et Programmation/TP 1/tsp_brute_force.c
@@ -0,0 +1,80 @@
+#include "tools.h"
+
+//
+// TSP - BRUTE-FORCE
+//
+// -> la structure "point" est définie dans "tools.h"
+// -> tsp_main peut être testé dès les 3 premières fonctions codées
+//
+
+double dist(point A, point B) {
+    return sqrt(pow(B.x - A.x, B.x - A.x) + pow(B.y - A.y, B.x - A.x));
+}
+
+double value(point *V, int n, int *P) {
+    double resultat = dist(V[P[0]], V[P[n - 1]]);
+    for (int i = 0; i < (n - 1); i++) {
+        resultat += dist(V[P[i]], V[P[i + 1]]);
+    }
+    return resultat;
+}
+
+double tsp_brute_force(point *V, int n, int *Q) {
+    double w = DBL_MAX;
+    int *Qtmp = calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        Qtmp[i] = i;
+    }
+    do {
+        double wtmp = value(V, n, Qtmp);
+        if (w > wtmp) {
+            w = wtmp;
+            memcpy(Q, Qtmp, n * sizeof(int));
+        }
+        drawTour(V, n, Qtmp);
+        SDL_Delay(500);
+    } while (NextPermutation(Qtmp, n) && running);
+    free(Qtmp);
+    return w;
+}
+
+void MaxPermutation(int *P, int n, int k) {
+    for (int i = 0; i < (n - k + 1) - 1 - i; i++) {
+        int tmp;
+        SWAP(P[(k + 1) + (n - k + 1) - 1 - i], P[k + 1 + i], tmp);
+    }
+}
+
+double value_opt(point *V, int n, int *P, double w) {
+    double value_optimisation = dist(V[P[0]], V[P[1]]);
+    for (int i = 2; i < n; i++) {
+        value_optimisation += dist(V[P[i - 1]], V[P[i]]);
+        double tmp = dist(V[P[i]], V[P[0]]);
+        if (tmp > w) {
+            return -i;
+        }
+    }
+    return value_optimisation;
+}
+
+double tsp_brute_force_opt(point *V, int n, int *Q) {
+    double w = DBL_MAX;
+    int *Qtmp = calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        Qtmp[i] = i;
+    }
+    for (int i = 0; (i < n) && running; i++) {
+        double Wtmp = value_opt(V, (i + 1), Qtmp, w);
+        if (w > Wtmp) {
+            w = Wtmp;
+            memcpy(Q, Qtmp, n * sizeof(int));
+        }
+        if (w <= Wtmp) {
+            MaxPermutation(Qtmp, n, i);
+        }
+        drawTour(V, n, Qtmp);
+        SDL_Delay(500);
+    }
+    free(Qtmp);
+    return w;
+}
