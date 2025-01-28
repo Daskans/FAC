18e1a1f212a6acf322960a1a4317ad2727976c11
Kyrian Balavoine <kyrian.balavoine@etu.u-bordeaux.fr>
2025-01-28 15:10:40 +0100

    PDL - get and post

--- L3 Info/Projet de Développement Logiciel/frontend/src/components/HelloWorld.vue
+++ L3 Info/Projet de Développement Logiciel/frontend/src/components/HelloWorld.vue
@@ -10,6 +10,7 @@ interface Image {
 defineProps<{ msg: string }>()
 const images = ref(<Image[]>([]));
 const selectedImage = ref("");
+const selectedFile = ref<File | null>(null);
 const imageElement = ref<HTMLImageElement | null>(null);
 
 
@@ -23,19 +24,50 @@ const getImageList = async () => {
   }
 }
 
-const downloadImage = async (imageId: number) => {
-  
+const downloadImage = async () => {
+  const imageUrl = `/images/${selectedImage.value}`;
+  console.log("Image URL => ",imageUrl);
+  console.log("Image ID => ",selectedImage.value);
+  console.log("Image element => ",imageElement.value);
   try {
-    const response = await axios.get(`/images/${selectedImage.value}`, { responseType: 'blob' });
+    const response = await axios.get(imageUrl, { responseType: 'blob' });
     const reader = new window.FileReader();
     reader.readAsDataURL(response.data);
     reader.onload = function() {
-
+      const imageUrl = reader.result;
+      if (imageElement.value && imageUrl) {
+        imageElement.value.src = imageUrl.toString();
+      }
     }
   } catch (error) {
     console.error(error);
   }
 }
+
+const submitFile = async () => {
+  if (selectedFile.value) {
+    const formData = new FormData();
+    formData.append('file', selectedFile.value);
+    try {
+      const response = await axios.post('/images', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+const handleFileUpload = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files.length > 0) {
+    selectedFile.value = target.files[0];
+  }
+}
+
 </script>
 
 <template>
@@ -43,11 +75,28 @@ const downloadImage = async (imageId: number) => {
 
   <button @click="getImageList()">lézimaj</button>
   <div>
-    <select>
+    <select v-model = "selectedImage">
       <option v-for="image in images" :key="image.id" :value="image.id">
         {{ image.name }}
       </option>
     </select>
+  </div>
+  <div>
+    <button @click="downloadImage()">Download</button>
+  </div>
+  <div class = "imageContainer">
+    <img ref="imageElement" class = "mainImage" />
+  </div>
+  <div class="container">
+    <div>
+      <h2>Single File</h2>
+      <hr/>
+      <label>File
+        <input type="file" @change="handleFileUpload"/>
+      </label>
+      <br>
+      <button v-on:click="submitFile()">Submit</button>
+    </div>
   </div>
 </template>
 
@@ -67,4 +116,26 @@ button {
 select {
   margin-top: 20px;
 }
+
+.imageContainer {
+  margin-top: 20px;
+}
+
+.mainImage {
+  max-width: 100%;
+  height: auto;
+}
+
+.container {
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+}
+
+input[type="file"] {
+  padding: 10px;
+}
+
 </style>
