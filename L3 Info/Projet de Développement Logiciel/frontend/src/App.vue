247b7b5aa7adb285db6704a13c8f0d30b1fdb851
Daskans <glpstkf123@gmail.com>
2025-01-31 17:33:50 +0100

    PDL - Failure at home

--- L3 Info/Projet de Développement Logiciel/frontend/src/App.vue
+++ L3 Info/Projet de Développement Logiciel/frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/ImageSingletonHandler.vue'
+import HelloWorld2 from './components/ImageGallery.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <HelloWorld2 />
 </template>
 
 <style scoped>
---
+++ L3 Info/Projet de Développement Logiciel/frontend/src/components/ImageGallery.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+import axios from 'axios';
+import downloadImage from './ImageSingletonHandler.vue';
+import images from './ImageSingletonHandler.vue';
+
+const getImageList = async () => {
+}
+
+</script>
+
+<template>
+    <div>
+        <ul>
+            <div class="container">
+                <button @click="getImageList()">Show Image Gallery</button>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+}
+
+button {
+  background-color: #9499be;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+</style>
